045f5e5474e6b8e6d550b89fa4cb1d6709d624b0
Kardo Heydari <120682793+imkardo@users.noreply.github.com>
2023-08-03 11:05:00 +0330

    Make Some Basic Changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,27 @@
 let productsContainer = document.querySelector("#products");
 let cartHeader = document.querySelector(".cart-header");
-let cart = document.querySelector(".cart");
+let cartContainer = document.querySelector(".cart");
 
 function showAllProducts() {
   for (let i = 0; i < products.length; i++) {
-    productsContainer.innerHTML += ` <div class="product-item">
-  <div class="product-image"><img src="` + products[i].imgSrc + `" alt="shoes"></div>
-  <div class="product-title">`+ products[i].name +`</div>
-  <div class="product-instock">تعداد موجود : `+ products[i].instock +`</div>
+    productsContainer.innerHTML +=
+      ` <div class="product-item">
+  <div class="product-image"><img src="` +
+      products[i].imgSrc +
+      `" alt="shoes"></div>
+  <div class="product-title">` +
+      products[i].name +
+      `</div>
+  <div class="product-instock">تعداد موجود : ` +
+      products[i].instock +
+      `</div>
   <div class="product-data">
-      <div class="product-price">`+ products[i].price +`</div>
-      <div class="add-to-cart" onclick="addToCart( `+ products[i].id +` )"><i class="fa-solid fa-cart-shopping"></i></div>
+      <div class="product-price">` +
+      products[i].price +
+      `</div>
+      <div class="add-to-cart" onclick="addToCart( ` +
+      products[i].id +
+      ` )"><i class="fa-solid fa-cart-shopping"></i></div>
   </div>
 </div>`;
   }
@@ -18,19 +29,23 @@ function showAllProducts() {
 showAllProducts();
 
 let n = 0;
-cartHeader.addEventListener('click',function(){
-  if(n==0){
-    cart.style.bottom = '0';
+cartHeader.addEventListener("click", function () {
+  if (n == 0) {
+    cartContainer.style.bottom = "0px"; // Show Cart
     n++;
-  }else{
-    cart.style.bottom = '-329px';
+  } else {
+    cartContainer.style.bottom = "-330px"; // Hide Cart
     n = 0;
   }
-
 });
+
+// Cart Array
+let cart = [];
+
 
 // add to cart function
 
 function addToCart(id){
-  console.log("product " + id + ' added to cart')
+cart.push(id)
+console.log(cart)
 }
